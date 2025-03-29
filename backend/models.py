ee876458bb3bb4e09b9603b1c8a7abedae1e0865
Nipuna Prabashwara <nipuna609@gmail.com>
2025-03-29 22:01:50 +0530

    User model update with bcryp add new two function for set_password, and check password

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,9 @@
 from database import db
 from datetime import datetime
 from enum import Enum
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 # Enum for scan severity levels
 class SeverityLevel(Enum):
@@ -12,8 +15,15 @@ class SeverityLevel(Enum):
 # User Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def set_password (self,password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password (self,password):
+        return bcrypt.check_password_hash(self.password_hash,password)
+    
     role = db.Column(db.Enum("user", "admin", name="user_roles"), default="user")
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     scans = db.relationship("Scan", backref="user", lazy=True)
