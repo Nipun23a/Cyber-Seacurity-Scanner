7e7a46beabbdfe2fe1bd36635cdfad93a4a8d2e9
Nipuna Prabashwara <nipuna609@gmail.com>
2025-03-29 21:37:47 +0530

    databse status added

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Initialize Database
+from database import db
+db.init_app(app)
+from models import *
+
+migrate = Migrate(app, db)  # Enable database migrations
 
 @app.route('/')
 def home():
