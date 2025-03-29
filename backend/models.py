696b6a2568efbd6f5732b3825fa24c14fc2453b6
Nipuna Prabashwara <nipuna609@gmail.com>
2025-03-29 21:27:41 +0530

    Database models created

---
+++ backend/models.py
@@ -0,0 +1,75 @@
+from database import db
+from datetime import datetime
+from enum import Enum
+
+# Enum for scan severity levels
+class SeverityLevel(Enum):
+    LOW = 'low'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+    CRITICAL = 'critical'
+
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique = True,nullable = False)
+    password = db.Column(db.String(255), nullable = False)
+    role = db.Column(db.Enum("user","admin",name = "user_roles"),default = "user")
+    created_at = db.Column(db.DateTime,default = datetime.now)
+    
+    scan = db.relationship("Scan",backref = "user",lazy = True)
+    files = db.relationship("File",backref = "user", lazy = True)
+    activity_logs = db.relationship('ActivityLog',backref = "user",lazy = True)
+    notifications = db.relationship('Notification',backref = "user",lazy = True)
+# Scan Model
+class Scan(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable = False)
+    device_name = db.Column(db.String(255),nullable = False)
+    scan_type = db.Column(db.Enum("malware","vulnerability",name= "scan_type"),nullable = False)
+    timestamp = db.Column(db.DateTime,default = datetime.utcnow)
+    scan_result = db.Column(db.JSON)
+    severity = db.Column(db.Enum(SeverityLevel),nullable = True)
+
+    scan_details = db.relationship("ScanDetail",backref = "scan",lazy = True)
+    recommendations = db.relationship('Recommendation',backref = "scan",lazy =True)
+    
+# Scan Details Model
+class ScanDetails(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    scan_id = db.Column(db.Integer,db.ForeignKey('scan.id'),nullable =False)
+    issue_type = db.Column(db.String(255),nullable = False)
+    description = db.Column(db.String(255),nullable = False)
+    severity = db.Column(db.Enum(SeverityLevel),nullable = False)
+    detected_at = db.Column(db.DateTime,default = datetime.utcnow)
+
+# Recommendation Model
+class Recommendations(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    scan_id = db.Column(db.Integer,db.ForeignKey('scan.id'),nullable = False)
+    issue = db.Column(db.String(255),nullable = False)
+    severity = db.Column(db.Enum(SeverityLevel),nullable = False)
+    solution = db.Column(db.Text,nullable = False)
+
+# File Upload Model
+class Files(db.Model):
+    id = db.Column(db.Integer,primary_key= True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable = False)
+    scan_id = db.Column(db.Integer,db.ForeignKey('scan.id'),nullable = False)
+    file_path = db.Column(db.String(255),nullable = False)
+    upload_at = db.Column(db.DateTime,default = datetime.utcnow)
+
+# Activity Log Model
+class ActivityLog(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable = False)
+    action = db.Column(db.String(255),nullable = False)
+    timestamp = db.Column(db.DateTime,default  = datetime.utcnow)
+
+# Notification Model
+class Notification(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable = False)
+    message = db.Column(db.Text,nullable = False)
+    status = db.Column(db.Enum('unread','read'),default = 'unread')
+    timestamp = db.Column(db.Datetime,default = datetime.utcnow)
