7d6b853d2ee6c6ca0dc08e862fff5c3cf9c64cca
Nipuna Kaweya <nipuna609@gmail.com>
2025-04-06 10:03:57 +0530

    Authentication Route Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,68 @@ import '@fontsource/open-sans/400.css'; // Regular weight
 import '@fontsource/open-sans/600.css'; // Semi-Bold weight
 import '@fontsource/open-sans/700.css'; // Bold weight
 
+import { useEffect, useState } from 'react';
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
+import Dashboard from "@/pages/Dashboard.tsx";
+import DashboardLayout from "@/layouts/dashboardLayout.tsx";
+
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+            setIsAuthenticated(true);
+        }
+    }, []);
+
+
+    // Function to handle successful login
+    const handleLoginSuccess = (token : string) => {
+        localStorage.setItem('authToken', token);
+    }
+
+    // Function to handle logout
+    const handleLoout = () => {
+        localStorage.removeItem('authToken');
+        setIsAuthenticated(false);
+    }
+    
     return (
-            <AuthForm />
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path = '/auth'
+                    element = {
+                        isAuthenticated ?
+                            <Navigate to="/dashboard" replace /> :
+                            <AuthForm onLoginSuccess={handleLoginSuccess} />
+                    }
+                />
+
+                {/* Dashboard Routes - protected by authentication */}
+                <Route
+                    path = '/dashboard'
+                    element = {
+                        isAuthenticated ?
+                            <DashboardLayout/> :
+                            <Navigate to="/auth" replace />
+                    }
+                />
+
+                <Route
+                    path="/"
+                    element={
+                        isAuthenticated ?
+                            <Navigate to="/dashboard" replace /> :
+                            <Navigate to="/auth" replace />
+                    }
+                />
+
+                {/* Catch all - redirect to appropriate location */}
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
